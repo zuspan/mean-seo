ee3537ee756f1463b4342cba86ccb3fb978362b6
Peter Zuspan <peter@kokowa.co>
2016-06-01 21:00:05 -0400

    slimertest 2

---
+++ lib/bin/local.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo 'working..'
+xvfb :1337 & export DISPLAY=:1337 & $1 $2 $3 2> node_modules/mean-seo/lib/bin/xvfb.log 
+
+# errormessage=$( xvfb :1337 & export DISPLAY=:1337 & $1 $2 2> &1)
+# echo $errormessage
+
+
+#echo 'finished'
+exit 0
