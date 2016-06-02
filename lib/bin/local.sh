f22569c508631d830088f87ef13e45963b88f37e
Peter Zuspan <peter@kokowa.co>
2016-06-01 22:52:30 -0400

    slimertest 6

--- lib/bin/local.sh
+++ lib/bin/local.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'working..'
-xvfb :1337 & export DISPLAY=:1337 & $1 $2 $3 2> node_modules/mean-seo/lib/bin/xvfb.log 
+xvfb :1337 -ac -screen 0 1280x1024x24 +extension GLX +render -noreset & export DISPLAY=:1337 & $1 $2 $3 2> node_modules/mean-seo/lib/bin/xvfb.log 
 
 # errormessage=$( xvfb :1337 & export DISPLAY=:1337 & $1 $2 2> &1)
 # echo $errormessage
